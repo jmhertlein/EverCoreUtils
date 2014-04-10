0144831b8e6da95135bc8ed8cb15cc4edb5c9f17
Joshua M Hertlein <jmhertlein@gmail.com>
2014-04-10 18:30:09 -0500

    Refactored LabeledKeyPair
    Now saves to YAML files/strings, and has working equals() implementation
    Also has unit tests

--- src/main/java/net/jmhertlein/core/crypto/persist/LabeledKeyPair.java
+++ src/main/java/net/jmhertlein/core/crypto/persist/LabeledKeyPair.java
@@ -6,16 +6,22 @@
 package net.jmhertlein.core.crypto.persist;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 import net.jmhertlein.core.crypto.Keys;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * An object representing a KeyPair with an associated label.
@@ -24,68 +30,111 @@ import net.jmhertlein.core.crypto.Keys;
  *
  * @author joshua
  */
-public class LabeledKeyPair extends LabeledPublicKey {
+public class LabeledKeyPair {
     private KeyPair keypair;
+    private String label;
 
     public LabeledKeyPair(String label, KeyPair keypair) {
-        super(label, keypair.getPublic());
+        this.label = label;
         this.keypair = keypair;
+    }
+
+    public String saveToYamlString() {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml yaml = new Yaml(options);
+
+        Map<String, Object> m = new HashMap<>();
+        m.put("label", label);
+        m.put("public", Keys.getBASE64ForKey(keypair.getPublic()));
+        m.put("private", Keys.getBASE64ForKey(keypair.getPrivate()));
+
+        return yaml.dump(m);
+    }
+
+    public void writeToYamlFile(File f) throws IOException {
+        if(!f.exists())
+            f.createNewFile();
+
+        try (PrintWriter pw = new PrintWriter(f)) {
+            pw.write(saveToYamlString());
+        }
+
+    }
+
+    public static LabeledKeyPair readFromYamlFile(File f) throws IOException {
+        String s;
+
+        try(FileInputStream fis = new FileInputStream(f); Scanner scan = new Scanner(fis)) {
+            scan.useDelimiter("\\Z");
+            s = scan.next();
+        }
+
+        return readFromYamlString(s);
+    }
+
+    public static LabeledKeyPair readFromYamlString(String s) {
+        Yaml yaml = new Yaml();
+        Map<String, Object> m = (Map<String, Object>) yaml.load(s);
+
+        return new LabeledKeyPair((String) m.get("label"),
+                                                 new KeyPair(Keys.getPublicKeyFromBASE64X509Encoded((String) m.get("public")),
+                                                             Keys.getPrivateKeyFromBASE64PKCS8Encoded((String) m.get("private"))));
     }
 
     public KeyPair getKeypair() {
         return keypair;
     }
 
-    @Override
-    public void writeToFile(File f) throws IOException {
-        if (!f.exists())
-            f.createNewFile();
 
-        try (PrintWriter pw = new PrintWriter(f)) {
-            pw.println("#Label:");
-            pw.println(getLabel());
-            pw.println("#Public key:");
-            pw.println(Keys.getBASE64ForKey(keypair.getPublic()));
-            pw.println("#Private key:");
-            pw.println(Keys.getBASE64ForKey(keypair.getPrivate()));
-        }
-    }
 
-    public static LabeledKeyPair readFromFile(File f) throws FileNotFoundException {
-        Scanner scan = new Scanner(f);
-
-        scan.nextLine(); //ignore commented line in input
-        String label = scan.nextLine().trim();
-        scan.nextLine(); //ignore commented line in input
-        PublicKey pub = Keys.getPublicKeyFromBASE64X509Encoded(scan.nextLine().trim());
-        scan.nextLine(); //ignore commented line in input
-        PrivateKey priv = Keys.getPrivateKeyFromBASE64PKCS8Encoded(scan.nextLine().trim());
-
-        return new LabeledKeyPair(label, new KeyPair(pub, priv));
-    }
-
-    @Override
-    public void writeToFile(String filename) throws IOException {
-        writeToFile(new File(filename));
-    }
-
-    public static LabeledKeyPair readFromFile(String filename) throws FileNotFoundException {
-        return readFromFile(new File(filename));
-    }
-
-    public static List<LabeledKeyPair> loadKeyPairsInDir(File dir) throws FileNotFoundException {
+    public static List<LabeledKeyPair> loadKeyPairsInDir(File dir) throws IOException {
         List<LabeledKeyPair> ret = new LinkedList<>();
 
         for (File f : dir.listFiles()) {
-            if (f.getName().endsWith(".lkp"))
-                ret.add(readFromFile(f));
+            if (f.getName().endsWith(".yml"))
+                ret.add(readFromYamlFile(f));
         }
 
         return ret;
     }
 
     @Override
-    public String getExtension() {
-        return ".lkp";
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.keypair);
+        hash = 37 * hash + Objects.hashCode(this.label);
+        return hash;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final LabeledKeyPair other = (LabeledKeyPair) obj;
+
+        if(!other.keypair.getPrivate().equals(keypair.getPrivate()))
+            return false;
+        if(!other.keypair.getPublic().equals(keypair.getPublic()))
+            return false;
+        if(!other.label.equals(label))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        b.append("=====LabeledKeyPair=======\n");
+        b.append("Pubkey:\n");
+        b.append(Keys.getBASE64ForKey(keypair.getPublic()));
+        b.append("\nPrivate key:\n");
+        b.append(Keys.getBASE64ForKey(keypair.getPrivate()));
+        b.append("\n==========================");
+        return b.toString();
+    }
+
+
 }
---
+++ src/test/java/net/jmhertlein/core/crypto/persist/test/LabeledKeyPairTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014 Joshua M Hertlein
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.jmhertlein.core.crypto.persist.test;
+
+import java.security.KeyPair;
+import net.jmhertlein.core.crypto.Keys;
+import net.jmhertlein.core.crypto.persist.LabeledKeyPair;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joshua
+ */
+public class LabeledKeyPairTest {
+    @Test
+    public void testPersistance() {
+        KeyPair pair = Keys.newRSAKeyPair(1024);
+        LabeledKeyPair p = new LabeledKeyPair("label", pair);
+
+        String yaml = p.saveToYamlString();
+
+        LabeledKeyPair read = LabeledKeyPair.readFromYamlString(yaml);
+        System.out.println(p);
+        System.out.println(read);
+        assertTrue(read.equals(p));
+    }
+
+    @Test
+    public void testEquals() {
+        final int KEY_LENGTH = 1024;
+        KeyPair a = Keys.newRSAKeyPair(KEY_LENGTH),
+                b = Keys.newRSAKeyPair(KEY_LENGTH),
+                c = Keys.newRSAKeyPair(KEY_LENGTH);
+
+        LabeledKeyPair w = new LabeledKeyPair("a", a),
+                       x = new LabeledKeyPair("a", a),
+                       y = new LabeledKeyPair("b", b),
+                       z = new LabeledKeyPair("c", c);
+
+        assertTrue(w.equals(w));
+        assertTrue(w.equals(x));
+        assertFalse(w.equals(y));
+        assertFalse(w.equals(z));
+
+        assertFalse(y.equals(z));
+    }
+}
